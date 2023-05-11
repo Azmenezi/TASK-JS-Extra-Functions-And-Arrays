3cbb3fe5bf2bb7f0fd6ae1d1a50018673f3ea9c3
Azmenezi <azmenezi@gmail.com>
2023-05-11 18:20:47 +0100

    Added An array checker in addNewTodo function

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ let doneTodos = []; //["Finish all coded tasks", "Sent important email"];
 
 const current = (newParamCurrent) => currentTodos.indexOf(newParamCurrent);
 const done = (newParamDone) => doneTodos.indexOf(newParamDone);
+
 const todosShow = (currentTodos2, doneTodos2) => {
-  currentTodos = currentTodos2;
-  doneTodos = doneTodos2;
-  console.log(`Current todos \n${currentTodos}`);
-  console.log(`Done todos \n${doneTodos}`);
+  if (currentTodos2 && doneTodos2) {
+    currentTodos = currentTodos2;
+    doneTodos = doneTodos2;
+    console.log(`Current todos \n${currentTodos}`);
+    console.log(`Done todos \n${doneTodos}`);
+  } else {
+    console.log(`Current todos \n${currentTodos}`);
+    console.log(`Done todos \n${doneTodos}`);
+  }
 };
 
 todosShow(
@@ -44,7 +50,20 @@ console.log(validateTodo("test"));
 
 const addNewTodo = (todo) => {
   //console.log(current);
-  if (current(todo) < 0 && done(todo) < 0 && validateTodo(todo) == true) {
+  if (
+    current(todo) < 0 &&
+    done(todo) < 0 &&
+    validateTodo(todo) == true &&
+    Array.isArray(todo)
+  ) {
+    currentTodos.push(...todo);
+    console.log(currentTodos);
+    console.log(doneTodos);
+  } else if (
+    current(todo) < 0 &&
+    done(todo) < 0 &&
+    validateTodo(todo) == true
+  ) {
     currentTodos.push(todo);
     console.log(currentTodos);
     console.log(doneTodos);
@@ -58,38 +77,40 @@ const addNewTodo = (todo) => {
     console.log("The task is already there");
   }
 };
+addNewTodo(["retest", "new"]);
+// addNewTodo("test");
+// addNewTodo("t");
+// addNewTodo("Review functions");
+// addNewTodo("Get Dry cleaning");
 
-addNewTodo("test");
-addNewTodo("t");
-addNewTodo("Review functions");
-addNewTodo("Get Dry cleaning");
+// const uncheckTodo = (undo) => {
+//   let newDone = done(undo);
+//   if (done(undo) >= 0) {
+//     doneTodos.splice(newDone, 1);
+//     currentTodos.push(undo);
+//     console.log(currentTodos);
+//     console.log(doneTodos);
+//   } else if (current(undo) >= 0) {
+//     console.log("You are not done with this task yet");
+//   } else {
+//     console.log("there is no task done with that name");
+//   }
+// };
 
-const uncheckTodo = (undo) => {
-  let newDone = done(undo);
-  if (done(undo) >= 0) {
-    doneTodos.splice(newDone, 1);
-    currentTodos.push(undo);
-    console.log(currentTodos);
-    console.log(doneTodos);
-  } else if (current(undo) >= 0) {
-    console.log("You are not done with this task yet");
-  } else {
-    console.log("there is no task done with that name");
-  }
-};
+// uncheckTodo("Finish all coded tasks");
+// uncheckTodo("test");
+// uncheckTodo("jshg");
 
-uncheckTodo("Finish all coded tasks");
-uncheckTodo("test");
-uncheckTodo("jshg");
+// const editTodo = (index, todoList, updatedTodo) => {
+//   let place = todoList.indexOf(index);
+//   if (place >= 0) {
+//     todoList[place] = updatedTodo;
+//     console.log(todoList);
+//   } else {
+//     console.log("there is no task with the same naming");
+//   }
+// };
 
-const editTodo = (index, todoList, updatedTodo) => {
-  let place = todoList.indexOf(index);
-  if (place >= 0) {
-    todoList[place] = updatedTodo;
-    console.log(todoList);
-  } else {
-    console.log("there is no task with the same naming");
-  }
-};
+// editTodo("test", currentTodos, "New To do");
 
-editTodo("test", currentTodos, "New To do");
+// todosShow();
